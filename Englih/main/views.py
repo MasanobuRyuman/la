a1de2237feba78335dd9df2727c51350accbd28a
ryumanmasanbou <22011376mr@tama.ac.jp>
2021-01-20 17:18:30 +0900

    ok

--- Englih/main/views.py
+++ Englih/main/views.py
@@ -4,3 +4,20 @@ from django.views import generic
 class IndexView(generic.TemplateView):
     template_name="index.html"
 # Create your views here.
+
+def exercise(request):
+    text = 'テストです。'
+    print(request.POST)
+    try:
+        input_text = request.POST['text_input']
+    
+        text = input_text
+    except:
+        return render(request, 'index.html')
+    print("ok")
+
+    
+    context = {
+        'text': text,
+    }
+    return render(request, 'index.html', context)
