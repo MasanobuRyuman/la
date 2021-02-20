c0c13355604fcf3666235132c7aa83dd50819722
ryumanmasanobu <22011376mr@tama.ac.jp>
2021-02-20 12:24:59 +0900

    form画面などを追加したi

--- Englih/main/urls.py
+++ Englih/main/urls.py
@@ -3,13 +3,15 @@ from . import views
 
 app_name='main'
 urlpatterns=[
+    path('',views.formView.as_view(),name='form'),
     path('exercise', views.exercise, name='exercise'),
     path('move',views.move,name='move'),
     path('delete',views.delete,name='delete'),
     path('next',views.next,name='next'),
     path('change',views.change,name='change'),
-    path('',views.IndexView.as_view(),name='index'),
     path('review',views.review,name='review'),
     path('test',views.test,name='test'),
     path('index',views.Index2View.as_view(),name='index'),
+    path('login',views.login,name='login'),
+    path('newlogin',views.newlogin,name='newlogin'),
 ]
